623c9644b394885425dfadc808343c7e550c38ea
DanteGhisi <ghisidante@gmail.com>
2026-04-13 20:11:36 -0300

    Post: Crear usuarios

--- controllers/usuarios_controllers.py
+++ controllers/usuarios_controllers.py
@@ -1,9 +1,34 @@
 from flask import request, jsonify
+from db import execute
+from errors import ERRORS
+
 
 def crear_usuario():
     # TODO: Leer body (nombre, email). No requiere auth 
-    return jsonify({"mensaje": "Usuario creado"}), 201
+    data = request.get_json()
+    
+    #Obtener los campos
+    nombre = data.get("nombre")
+    email = data.get("email")
+    
+    #Validar campos obligatorios
+    if not nombre or not email:
+        return ERRORS["MISSING_REQUIRED_FIELDS"]("Nombre y email son requeridos")
+    
+    #Verificar si el email ya existe
+    query = f"SELECT * FROM usuarios WHERE email = '{email}'"
+    response = execute(query)
 
+    if response == False: return ERRORS["UNKNOWN_ERROR"]("Error al obtener usuarios")
+
+    if len(response) != 0: return ERRORS["CONFLICT"]("Email ya registrado")
+    
+    #Insertar nuevo usuario
+    query = f"INSERT INTO usuarios (nombre, email) VALUES ('{nombre}', '{email}')"
+    response = execute(query)
+    if response == False: return ERRORS["UNKNOWN_ERROR"]("Error al crear usuario")
+    return "", 201
+    
 def listar_usuarios():
     # TODO: Soportar paginación (limit, offset) 
     return jsonify({"mensaje": "Lista de usuarios"}), 200
--- db.py
+++ db.py
@@ -17,10 +17,11 @@ def get_db_connection():
     conn = mysql.connector.connect(**db_config)
     return conn
 
+
 def execute(query):
+    conexion = get_db_connection()
+    cursor = conexion.cursor(dictionary=True) # dictionary=True para que devuelva dicts en vez de tuplas
     try:
-        conexion = get_db_connection()
-        cursor = conexion.cursor(dictionary=True) # dictionary=True para que devuelva dicts en vez de tuplas
         cursor.execute(query)
         conexion.commit()
         resultados = cursor.fetchall()
