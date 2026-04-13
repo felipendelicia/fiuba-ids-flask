efec6b4186133dde2d783302e4307d3822ae2969
felipendelicia <delicia4581@gmail.com>
2026-04-13 15:43:22 -0300

    mysql docker compose

--- routes/partidos_routes.py
+++ routes/partidos_routes.py
@@ -3,16 +3,16 @@ from controllers.partidos_controllers import *
 
 partidos_bp = Blueprint('partidos', __name__)
 
-# Endpoints Obligatorios [cite: 70]
+# Endpoints Obligatorios 
 partidos_bp.route('/', methods=['GET'])(listar_partidos)
 partidos_bp.route('/', methods=['POST'])(crear_partido)
 partidos_bp.route('/<int:id>', methods=['GET'])(obtener_partido) 
 partidos_bp.route('/<int:id>', methods=['DELETE'])(eliminar_partido) 
 partidos_bp.route('/<int:id>/resultado', methods=['PUT'])(cargar_resultado) 
 
-# Predicciones (Enunciado Sorpresa) [cite: 198]
+# Predicciones (Enunciado Sorpresa)
 partidos_bp.route('/<int:id>/prediccion', methods=['POST'])(registrar_prediccion) 
 
-# Endpoints Opcionales [cite: 102]
+# Endpoints Opcionales 
 partidos_bp.route('/<int:id>', methods=['PUT'])(reemplazar_partido) 
 partidos_bp.route('/<int:id>', methods=['PATCH'])(actualizar_partido_parcial) 
